6a933d18d7aa72828c16456fbe6e2cb59aa4056d
devsalman247 <salman247.dev@gmail.com>
2023-01-08 00:05:28 +0500

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import transakSDK from "@transak/transak-sdk";
-const { VITE_WYRE_API_KEY, VITE_WYRE_SECRET_KEY, VITE_TRANSAK_API_KEY, VITE_TRANSAK_SECRET_KEY } = import.meta.env;
+const {
+	VITE_WYRE_API_KEY,
+	VITE_WYRE_SECRET_KEY,
+	VITE_TRANSAK_API_KEY,
+	VITE_TRANSAK_API_KEY_2,
+	VITE_TRANSAK_SECRET_KEY,
+} = import.meta.env;
 
 function App() {
 	const buy = () => {
 		let transak = new transakSDK({
-			apiKey: "71d9cc91-826d-41b6-b6ba-7d8962a9c3e0", // Your API Key
+			apiKey: VITE_TRANSAK_API_KEY_2, // Your API Key
 			environment: "STAGING", // STAGING/PRODUCTION
-			widgetHeight: "525px",
+			widgetHeight: "660px",
 			widgetWidth: "500px",
 			// Examples of some of the customization parameters you can pass
 			defaultCryptoCurrency: "MATIC", // Example 'ETH'
 			walletAddress: "0x59EF24f97230DC13Fe8B4dD9f35a757e0a1Ad29b", // Your customer's wallet address
 			// themeColor: "[COLOR_HEX]", // App theme color
-			fiatCurrency: "GBP", // If you want to limit fiat selection eg 'GBP'
+			// fiatCurrency: "GBP", // If you want to limit fiat selection eg 'GBP'
 			email: "muhammadsalman2471006@gmail.com", // Your customer's email address
 			redirectURL: "http://localhost:5173/", // Redirect URL of your app
 		});
