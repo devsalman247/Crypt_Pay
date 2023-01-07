10e82518922f7b44f69f93ade2a95ef6fae2c2ff
devsalman247 <salman247.dev@gmail.com>
2023-01-08 00:29:55 +0500

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,14 @@ const {
 } = import.meta.env;
 
 function App() {
+	const [wallet_address, setWalletAddress] = useState("");
+	const [email, setEmail] = useState("");
+
 	const buy = () => {
+		if (!email || !wallet_address) {
+			alert("Please enter details!");
+			return;
+		}
 		let transak = new transakSDK({
 			apiKey: VITE_TRANSAK_API_KEY_2, // Your API Key
 			environment: "STAGING", // STAGING/PRODUCTION
@@ -17,10 +24,10 @@ function App() {
 			widgetWidth: "500px",
 			// Examples of some of the customization parameters you can pass
 			defaultCryptoCurrency: "MATIC", // Example 'ETH'
-			walletAddress: "0x59EF24f97230DC13Fe8B4dD9f35a757e0a1Ad29b", // Your customer's wallet address
+			walletAddress: wallet_address, // Your customer's wallet address
 			// themeColor: "[COLOR_HEX]", // App theme color
 			// fiatCurrency: "GBP", // If you want to limit fiat selection eg 'GBP'
-			email: "muhammadsalman2471006@gmail.com", // Your customer's email address
+			email, // Your customer's email address
 			redirectURL: "http://localhost:5173/", // Redirect URL of your app
 		});
 
@@ -44,15 +51,31 @@ function App() {
 
 	return (
 		<div className="flex justify-center items-center h-[100vh]">
-			{/* <a
-				href="https://global-stg.transak.com/?apiKey=71d9cc91-826d-41b6-b6ba-7d8962a9c3e0&cryptoCurrencyCode=MATIC"
+			<div className="bg-neutral-200 flex flex-col p-6 gap-4 rounded-md">
+				<label>Enter your email address:</label>
+				<input
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					className="outline-none px-2 py-1 rounded-sm border-blue-400 border"
+				/>
+				<label>Enter your wallet address:</label>
+				<input
+					type="text"
+					value={wallet_address}
+					onChange={(e) => setWalletAddress(e.target.value)}
+					className="outline-none px-2 py-1 rounded-sm border-blue-400 border"
+				/>
+				{/* <a
+				href="https://global-stg.transak.com/?apiKey=your_api_key&cryptoCurrencyCode=MATIC"
 				// onClick={() => buy()}
 				className="bg-slate-500 text-white text-lg px-2 py-1 rounded-md">
 				Buy Crypto
 			</a> */}
-			<button onClick={() => buy()} className="bg-slate-500 text-white text-lg px-2 py-1 rounded-md">
-				Buy Crypto
-			</button>
+				<button onClick={() => buy()} className="bg-slate-500 text-white text-lg px-2 py-1 rounded-md">
+					Buy Crypto
+				</button>
+			</div>
 		</div>
 	);
 }
